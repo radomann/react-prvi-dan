8302bf0dfa2656d20d114614f5ede5ce0ccd4cd0
Nemanja Radoman <nemanja.radoman@gmail.com>
2023-09-14 11:26:42 +0200

    day 3

--- src/components/Text.jsx
+++
@@ -1,7 +0,0 @@
-export const Text = ({ name="Aleksa", country="Poland" }) => {
-    return <>
-        <h1 htmlFor="saf" className='first-h1' style={{ backgroundColor: "lightsalmon", fontSize: 48 }}>Cao DevLab React JS Ekipo! 2023</h1>
-        <h2>{name} <span>Miks</span> - <span style={{ fontStyle: "italic" }}>{country}</span></h2>
-        <p dangerouslySetInnerHTML={{ __html: "<b>Ponedeljak</b>" }}></p>
-        </>
-}
